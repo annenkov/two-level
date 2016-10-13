ee3540b280b32a18a83abf770fb0b0a8b24b5ba1
Danil <danil.v.annenkov@gmail.com>
2016-10-13 11:04:47 +0100

    Removed redundant code, refactored lemma3 proof

--- reedy.lean
+++ reedy.lean
@@ -1,19 +1,12 @@
 import fibrant data.fin data.equiv
 
-open nat equiv function fin eq.ops
-
-definition pi_is_fibrant'' {X : Type}{Y : X → Type}
-(fibX : is_fibrant X) (fibY : Π (x : X), is_fibrant (Y x)) :
-is_fibrant (Π (x : X), Y x) :=
-is_fibrant.mk (pi_is_fibrant' fibX fibY)
-
-open sum equiv.ops
+open nat equiv function fin eq.ops sum equiv.ops unit prod.ops
 
 definition fam_indexed_by_empty_equiv_unit {X : empty → Type}: (Π i, X i) ≃ unit :=
-  equiv.mk (λ x, unit.star) (λ x, empty.rec X) 
-           begin unfold left_inverse, intro, apply funext, intro, cases x_1  end
-           begin 
-             intro, cases x, reflexivity  
+  equiv.mk (λ x, unit.star) (λ x, empty.rec X)
+           begin unfold left_inverse, intro x, apply funext, intro i, cases i end
+           begin
+             intro, cases x, reflexivity
            end
 
 definition fin_0_fam_equiv.{u} : (fin 0 → Type.{u}) ≃ (empty → Type.{u}) :=
@@ -22,46 +15,37 @@ definition fin_0_fam_equiv.{u} : (fin 0 → Type.{u}) ≃ (empty → Type.{u}) :
 definition pi_fin0_unit_equiv {X : fin 0 → Type} : (Π i, X i) ≃ unit :=
   equiv.mk (λ x, unit.star) (λ x i, fin.elim0 i)
   begin unfold left_inverse, intro, apply funext, intro, apply (elim0 x_1) end
-  begin 
+  begin
   intro, cases x, reflexivity
   end
 
-open unit prod.ops
-  
 lemma pi_sum_fin_unit_equiv {n} {X : (unit + fin n) → Type} : (Π i, X i) ≃ (X (inl ⋆) × Π i, X (inr i)) :=
-  equiv.mk (λ x, (x (inl ⋆), λ y, x (inr y))) (λ p, λ z, sum.cases_on z (λ x1, unit.cases_on x1 p.1) (λ x2, p.2 x2)) 
-  begin 
-    unfold left_inverse, esimp, intro, apply funext, intro, 
-    cases x_1, 
-      esimp, cases a, esimp, 
-      esimp
+  equiv.mk
+    (λ x, (x (inl ⋆), λ y, x (inr y)))
+    (λ p, λ z, sum.cases_on z (λ x1, unit.cases_on x1 p.1) (λ x2, p.2 x2))
+  begin
+    unfold left_inverse, esimp, intro, apply funext, intro i,
+    cases i,
+      { esimp, cases a, esimp },
+      { esimp }
   end
   begin
     unfold right_inverse, unfold left_inverse, apply prod.eta
   end
 
-definition pi_sum_fin_unit_equiv' {n} {Heq : fin n + unit ≃ fin (nat.succ n)}
-      {X : fin (nat.succ n) → Type} 
+definition pi_sum_fin_unit_equiv' {n} (Heq : fin n + unit ≃ fin (nat.succ n))
+      {X : fin (nat.succ n) → Type}
       : (Π i, X (Heq ∙ i)) ≃ (X (Heq ∙ (inr ⋆)) × Π i, X (Heq ∙ (inl i))) :=
- equiv.mk 
-   (λ x, (x (inr ⋆), λ y, x (inl y))) 
-   (λ p, λ z, sum.cases_on z (λ x2, p.2 x2) (λ x1, unit.cases_on x1 p.1)) 
-   begin 
-     unfold left_inverse, esimp, intro, apply funext, intro, 
-     cases x_1, esimp, cases a, esimp
+ equiv.mk
+   (λ x, (x (inr ⋆), λ y, x (inl y)))
+   (λ p, λ z, sum.cases_on z (λ x2, p.2 x2) (λ x1, unit.cases_on x1 p.1))
+   begin
+     unfold left_inverse, esimp, intro x, apply funext, intro i,
+     cases i, esimp, cases a, esimp
    end
    begin
      unfold right_inverse, unfold left_inverse, apply prod.eta
    end
-
-
-lemma equiv_id_left {A B : Type} [eq : A ≃ B] {i : A} : (@equiv.inv _ _ eq (equiv.fn eq i)) = i :=
-  begin apply equiv.left_inv end
-
-lemma equiv_id_right {A B : Type} [eq : A ≃ B] {i : B} : (equiv.fn eq (@equiv.inv _ _ eq i)) = i :=
-begin apply equiv.right_inv end
-
-set_option formatter.hide_full_terms false
 
 definition ap {A B : Type}(f : A → B){a a' : A}
               (p : a = a') : f a = f a' := p ▹ eq.refl _
@@ -75,7 +59,7 @@ definition naturality_subst {X Y : Type}{x x' : X}{P : Y → Type}
                             ap f p ▹ u = p ▹ u :=
   eq.drec (eq.refl _) p
 
-definition pi_congr {A A' : Type} [φ : equiv A A'] {B : A' → Type}
+definition pi_congr {A A' : Type} {B : A' → Type} (φ : equiv A A')
                        : (Π (a : A), B (φ ∙ a)) ≃ (Π (a : A'), B a) :=
   match φ with mk f g l r :=
   mk (λ k x', r x' ▹ k (g x'))
@@ -89,20 +73,17 @@ definition pi_congr {A A' : Type} [φ : equiv A A'] {B : A' → Type}
      (λ h, funext (λ x', apd h (r x')))
   end
 
-definition prod_is_fibrant'' [instance] {X Y : Type}
-{fibX : is_fibrant X} {fibY : is_fibrant Y} :
-is_fibrant (X × Y) := is_fibrant.mk (prod_is_fibrant' fibX fibY)
-
-definition lemma3 {n : ℕ}
-                  {X : fin n → Type}
+definition lemma3 {n : ℕ}  {X : fin n → Type}
                   {fibX : Π i, is_fibrant (X i)}
-  : is_fibrant (Π i, X i) :=
-begin
-  induction n,
-  apply equiv_is_fibrant, apply (equiv.symm pi_fin0_unit_equiv),
+           : is_fibrant (Π i, X i) :=
+  begin
+    induction n with n IHn,
+    { apply (equiv_is_fibrant (equiv.symm pi_fin0_unit_equiv)) },
 
-  have HeqFinSum : fin (succ a) ≃ fin a + unit, from (equiv.symm (fin_sum_unit_equiv a)),
-  apply equiv_is_fibrant, apply (@pi_congr _ _ HeqFinSum⁻¹ _),  apply equiv_is_fibrant, apply equiv.symm,
-  apply (@pi_sum_fin_unit_equiv' a (equiv.symm HeqFinSum) X), apply (@prod_is_fibrant'' _ _), apply fibX, apply v_0,
-  intro, apply fibX
-end
+    { have HeqFinSum : fin n + unit ≃ fin (succ n), from (fin_sum_unit_equiv n),
+      apply equiv_is_fibrant,
+      { apply (pi_congr HeqFinSum) },
+      { apply equiv_is_fibrant,
+        { apply (equiv.symm (pi_sum_fin_unit_equiv' HeqFinSum)) },
+        { apply @prod_is_fibrant, apply IHn, intro, apply fibX } }}
+  end
