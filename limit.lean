25568dc9324e12b05dd8d9c0fdc3aa7dfc62f207
Danil <danil.v.annenkov@gmail.com>
2016-10-19 14:20:54 +0100

    Started work on limits. Category of cones.

---
+++ limit.lean
@@ -0,0 +1,37 @@
+import algebra.category
+
+open category natural_transformation
+
+definition Δ {J C : Category} (c : C) : J ⇒ C :=
+  ⦃ functor,
+    object := λ i, c,
+    morphism := λ i j g, id,
+    respect_id := λ i, eq.refl _,
+    respect_comp := λi j k f g, by rewrite id_left ⦄
+
+definition cone {J C : Category} (D : J ⇒ C) := Σ c, Δ c ⟹ D
+
+open sigma.ops
+
+structure cone_hom {J C : Category} {D : J ⇒ C} (c : cone D ) (c' : cone D) : Type :=
+  (chom : c.1 ⟶ c'.1)
+  (commute_triangle : ∀ i, natural_map c.2 i = natural_map c'.2 i ∘ chom)
+
+open cone_hom
+
+definition cone_hom_eq {J C : Category} {D : J ⇒ C } {c c' : cone D}
+                       {f f': cone_hom c c'} (p : chom f = chom f') : f = f' :=
+  begin cases f, cases f', cases p, reflexivity end
+
+definition cone_hom_comp {J C : Category} {D : J ⇒ C } {c c' c'': cone D} (f' : cone_hom c' c'') (f : cone_hom c c') :=
+  ⦃ cone_hom, chom := chom f' ∘ chom f,
+    commute_triangle := λ i, by rewrite [assoc, -commute_triangle f', commute_triangle f] ⦄
+
+definition cone_category [instance] {J C : Category} {D : J ⇒ C} : category (cone D) :=
+  ⦃ category,
+    hom := λ c c', cone_hom c c',
+    comp := λ a b c f g, cone_hom_comp f g,
+    ID := λi, ⦃ cone_hom, chom := id, commute_triangle := λ i, by rewrite id_right ⦄,
+    assoc := λ a b c p q h g, by cases h; cases q; cases g; apply cone_hom_eq; apply assoc,
+    id_left := λ a b f, cone_hom_eq (id_left (chom f)),
+    id_right := λ a b f, cone_hom_eq (id_right (chom f)) ⦄
