9aef799fbad4b188f3936158fb51c35caa6c970e
Danil <danil.v.annenkov@gmail.com>
2016-11-14 16:23:02 +0000

    Changed variables declaration for pullback

--- fibrantlimits.lean
+++ fibrantlimits.lean
@@ -1,11 +1,11 @@
 import fibrant limit data.equiv
 
 section pullback
+
 universe variable u
-variables A B C : Type.{max 1 u}
-variable f : A → C
-variable isfib : is_fibration_alt f
-variable g : B → C
+variables {A B C : Type.{max 1 u}}
+          (f : A → C) (g : B → C)
+          {isfib : is_fibration_alt f}
 
 definition Pullback : Type :=
   Σ (a : A), Σ (b : B), Σ (c : C), (f a = c) × (g b = c)
@@ -14,7 +14,7 @@ definition Pullback' : Type := Σ (b : B), fibreₛ f (g b)
 
 open sigma.ops
 definition Pullback'_is_fibrant : is_fibration_alt
-  (λ (pb : Σ (b : B), fibreₛ f (g b)), pb.1)
+  (λ (pb : Pullback' f g), pb.1)
   := λ b, @equiv_is_fibrant _ _ (equiv.symm (fibre_projection b)) (isfib (g b))
 
 end pullback
