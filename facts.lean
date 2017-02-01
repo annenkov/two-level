aebb9b9fc68539b8e4b18dbda23366cb02694c4e
Danil <danil.v.annenkov@gmail.com>
2017-02-01 14:16:13 +0100

    Filled in missing bits about equivalence for pi and sigma

--- facts.lean
+++ facts.lean
@@ -18,6 +18,12 @@ namespace eq
     (f : X → Y)(p : x = x')(u : P (f x)) : ap f p ▹ u = p ▹ u :=
     eq.drec (eq.refl _) p
 
+  definition transfer_concat {A : Type } {a b c: A} (P : A → Type)
+    (p : a = b) (q : b = c) (u : P a) : q ▹ (p ▹ u) = p ⬝ q ▹ u := begin cases p, cases q, reflexivity end
+
+  definition concat_inv {A : Type } {a b : A} {P : A → Type}
+    (p : a = b) (u : P a) : #eq.ops p ⬝ p⁻¹ ▹ u = u := by cases p; reflexivity
+
 end eq
 
 open eq
@@ -42,7 +48,12 @@ namespace equiv
   end
 
   definition pi_congr₂ [instance] {F G : A → Type}  [φ : Π a, F a ≃ G a]
-    : (Π (a : A), F a) ≃ (Π (a : A), G a) := equiv.mk sorry sorry sorry sorry
+    : (Π (a : A), F a) ≃ (Π (a : A), G a) :=
+    equiv.mk
+      (λ x a, (φ a) ∙ x a)
+      (λ x a, inv (x a))
+      (λ x, funext (λ a, (left_inv _ (G a) _) ▹ rfl))
+      (λ x, funext (λ a, (right_inv (F a) _ _) ▹ rfl))
 
   definition pi_congr [instance] {F : A → Type} {F' : A' → Type} [φ : A ≃ A'] [φ' : Π a, F a ≃ F' (φ ∙ a)] :=
     equiv.trans (@pi_congr₂ _ _ _ φ') (@pi_congr₁ _ _ _ φ)
@@ -52,38 +63,53 @@ namespace equiv
       begin intro p, cases p with [p₁, p₂], cases p₁, cases p₂, esimp end
 
   definition sigma_congr₁ [instance] {A B: Type} {F : B → Type} [φ : A ≃ B]:
-  (Σ a : A, F (to_fun B a)) ≃ Σ b : B, F b :=
+  (Σ a : A, F (φ ∙ a)) ≃ Σ b : B, F b :=
   equiv.mk
-  (λ x , ⟨ _, x.2 ⟩ )
-  (λ x,  ⟨ _, (eq.symm (right_inv A B _)) ▹ x.2⟩ )
+  (λ x , ⟨ φ ∙ x.1, x.2 ⟩ )
+  (λ x,  ⟨ inv x.1, begin unfold fn, unfold inv, rewrite right_inv, apply x.2 end⟩ )
   (λ x, begin
+        unfold fn at *,
+        cases φ with [f, g, l, r], esimp at *, unfold fn,
         cases x with [x₁, x₂],
-        cases φ with [f, g, l, r], unfold function.right_inverse at *, unfold function.left_inverse at *, esimp,
-        apply sigma_eq_congr, refine ⟨_,_⟩, apply l,
+        unfold function.right_inverse at *, unfold function.left_inverse at *, unfold inv, esimp,
+        apply sigma_eq_congr, refine ⟨l x₁,_⟩,
         calc
-        (l x₁ ▹ #eq.ops ((r (f x₁))⁻¹ ▹ x₂))
-            = #eq.ops (r (f x₁))⁻¹ ▹ x₂ : sorry --apd (λ p, eq.rec x₂ (eq.symm(r (f x₁)))) (l x₁)
-        ... = x₂ : apd _ _
+        (l x₁ ▹ ((r (f x₁))⁻¹ ▹ x₂))
+            = (l x₁ ▹ (eq.symm (ap f (l x₁)) ▹ x₂)) : rfl
+        ... = (l x₁ ▹ (eq.symm (l x₁) ▹ x₂)) : naturality_subst
+        ... = ((l x₁ ⬝ (eq.symm (l x₁))) ▹ x₂) : transfer_concat
+        ... = x₂ : concat_inv (l x₁)
         end)
    begin
-     intro x, cases x with [p₁, p₂],
+     intro x, cases x with [x₁, x₂],
      cases φ with [f, g, l, r], unfold function.right_inverse at *, unfold function.left_inverse at *,  esimp at *,
-     apply sigma_eq_congr, refine ⟨_,_⟩, apply r,
-     have H0 : #eq.ops r p₁ ▹ (r p₁)⁻¹ ▹ p₂ = r p₁ ▹ (λ x, (r p₁)⁻¹ ▹ p₂) p₂, from rfl,
-     have H : #eq.ops r p₁ ▹ (r p₁)⁻¹ ▹ p₂ = @eq.rec _ _ (λa, F a) p₂ _ (r p₁)⁻¹, from apd _ _,
-     have H' : #eq.ops (r p₁)⁻¹ ▹ p₂ = p₂, from apd _ _,
-     calc #eq.ops r p₁ ▹ (r p₁)⁻¹ ▹ p₂
-         = r p₁ ▹ (λ x, (r p₁)⁻¹ ▹ p₂) p₂ : rfl
-     ... = #eq.ops (r p₁)⁻¹ ▹ p₂ : sorry --H
-     ... = p₂ : sorry
+     apply sigma_eq_congr, refine ⟨_,_⟩, apply r,     
+     calc
+      #eq.ops 
+      r x₁ ▹ (r x₁)⁻¹ ▹ x₂
+         = r x₁ ⬝ (r x₁)⁻¹ ▹ x₂ : transfer_concat
+     ... = x₂ : concat_inv (r x₁)
    end
 
   definition sigma_congr₂ [instance] {A : Type} {F G : A → Type} [φ : Π a : A, F a ≃ G a] :
     (Σ a, F a) ≃ Σ a, G a :=
-    equiv.mk sorry sorry sorry sorry
+    equiv.mk
+      (λ x, ⟨x.1, (φ x.1) ∙ x.2⟩)
+      (λ x, ⟨x.1, inv x.2⟩)
+      begin
+        unfold left_inverse, intros,
+        cases x with [p₁, p₂], esimp,
+        congruence, unfold fn, unfold inv,
+        rewrite left_inv
+      end
+      begin
+        unfold right_inverse, unfold left_inverse, intros,
+        cases x with [p₁, p₂], congruence, unfold fn, unfold inv,
+        rewrite right_inv
+      end
 
   definition sigma_congr {A B : Type} {F : A → Type} {G : B → Type}
-    [φ : A ≃ B] [φ' : Π a : A, F a ≃ G (to_fun B a)] :
+    [φ : A ≃ B] [φ' : Π a : A, F a ≃ G (φ ∙ a)] :
     (Σ a, F a) ≃ Σ a, G a := equiv.trans sigma_congr₂ sigma_congr₁
 
   definition sigma_swap {A B : Type} {F : A → B → Type} :
