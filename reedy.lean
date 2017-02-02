da4ed8b2ecad19d9f671890fc14d311c06e6a659
Danil <danil.v.annenkov@gmail.com>
2017-02-02 15:04:05 +0100

    Finished filling all the missing bits

--- reedy.lean
+++ reedy.lean
@@ -165,13 +165,16 @@ section reedy
       assert H : @eq (C_without_z z) x t, begin cases x, cases t, esimp at *, congruence, assumption end,      
       apply non_id H, cases H, esimp, apply q
     end
+    -- check @red_coslice_obs.cases_on
+    -- definition red_coslice_to_C'_β {t : C_without_z z} {f : x ⟶ t} {non_id : ∀ p, ¬p ▹ f = id} :
+    --   (red_coslice_to_C' (red_coslice_obs.mk t _ non_id)) = (red_coslice_obs.mk t _ _) := sorry
 
     definition red_coslice_to_C'_hom (a b : x//C_without_z z) (f : a ⟶ b) : (red_coslice_to_C' a) ⟶ (red_coslice_to_C' b) := 
     begin 
-    cases f with [f, comm_tr], cases a with [a,g,non_id], cases b with [b,g', non_id'], 
-    unfold red_coslice_obs.to_coslice_obs at *, unfold red_coslice_to_C' at *, esimp at *,
-    apply ⟨f,comm_tr⟩
-    end    
+      cases f with [f, comm_tr], cases a with [a,g,non_id], cases b with [b,g', non_id'], 
+      unfold red_coslice_obs.to_coslice_obs at *, unfold red_coslice_to_C' at *, esimp at *,
+      apply ⟨f,comm_tr⟩
+    end
 
     -- (Danil) I pack this property as a lemma to prevent unnessesary unfolding
     -- which sometimes leads to errors when unfolding definitions dependent on this one
@@ -220,28 +223,26 @@ section reedy
       cases y, esimp, unfold red_coslice_from_C', cases x, esimp
     end
 
-    definition red_coslice_eq_1 [reducible] [unfold_full] :
-      ∀ y, object X (obj (red_coslice_obs.to ((@red_coslice_from_C' C _ _ _ inj_φ max_rank _ _) y))) = X (red_coslice_obs.to y) := 
+    definition red_coslice_eq_1 :
+    ∀ y, object X (obj (red_coslice_obs.to ((@red_coslice_without_z_equiv  _ _ _ _ inj_φ max_rank _ _) ∙ y))) = 
+      X (red_coslice_obs.to y) := 
       begin
-        intro, cases y with [y,f,y_ne], cases x with [x, x_ne], esimp        
+        intro, unfold fn, cases y with [y,f,y_ne], cases x with [x, x_ne], esimp
       end
-    
-    -- definition MO_functors_tr_commutes1 : ∀ y, ((Functor_from_C' z X) ∘f !forget) 
-    --        (@to_fun _ _ @red_coslice_without_z_equiv C z x φ inj_φ max_rank _ _ y) = (X ∘f !forget) y :=
-    --   begin intro, unfold functor.compose, unfold forget, unfold Functor_from_C',
-    --     unfold red_coslice_obs.to_coslice_obs, esimp, refine red_coslice_eq_1 _ _, assumption
-    --   end
-    set_option pp.binder_types true
-    -- definition MO_functors_tr_commutes_hom {a b : (obj x)//C} :
-    -- ∀ f : hom a b, -- morphism (X ∘f forget C z) f
-    -- morphism ((Functor_from_C' z X) ∘f forget _ x) (red_coslice_from_C'_hom f) = _  := sorry
-    
+  
     definition MO'_to_MO_map : 
     matching_object (Functor_from_C' z X) x → matching_object X (obj x) :=
     begin
-      intros a, refine natural_transformation.mk _ _,
-      { intros y uu, unfold functor.compose at *, unfold forget at *, esimp at *, unfold red_coslice_obs.to_coslice_obs,
-        rewrite -(@red_coslice_eq_1 _ _ _ _ inj_φ max_rank _ _ X y), refine (natural_map a) _ poly_unit.star },
+      let  ψ := @red_coslice_without_z_equiv C z _ φ inj_φ max_rank _ _,
+      intros a,  --cases a with [η, NatSq], 
+      refine natural_transformation.mk _ _,
+      { intros y uu, unfold functor.compose at *, unfold forget at *, esimp at *, unfold red_coslice_obs.to_coslice_obs, 
+      unfold one_funct at *, unfold Functor_from_C' at *, 
+      have HH : object X (red_coslice_obs.to (ψ ∙ y)), from (natural_map a) (ψ ∙ y) star,
+      intro, unfold fn at *,
+      cases y with [y,f,y_ne], cases x with [x', x_ne], esimp at *, 
+      apply HH,
+      },
       { cases a with [η, NatSq], intros x' y,        
         intros f',
         let X' := Functor_from_C' z X,
@@ -250,48 +251,59 @@ section reedy
         morphism (X' ∘f forget C' x) (red_coslice_from_C'_hom f') ∘ η (red_coslice_from_C' x') = 
         η (@red_coslice_from_C' _ _ _ _ inj_φ max_rank _ _ y),
         begin apply NatSq _ end,
-        cases x with [x,f,non_id_f], cases y with [y,g,non_id_g], esimp at *,
-        --unfold red_coslice_obs.to_coslice_obs at *,
+        cases x with [x,f,non_id_f], cases y with [y,g,non_id_g], esimp at *,        
         cases f' with [a,p1], esimp,
         cases x' with [o, o_ne], esimp,
         unfold Functor_from_C' at *, unfold functor.compose at *, unfold forget at *, esimp at *,
         unfold red_coslice_from_C'_hom at *, esimp at *,
         rewrite natural_map_proj,
-        apply funext, intros uu, esimp, esimp, cases uu, refine happly HH _        
+        apply funext, intros uu, esimp, esimp, cases uu, refine happly HH _
          }
     end
-
+    --set_option pp.implicit true
     definition MO_equiv : matching_object (Functor_from_C' z X) x ≃ₛ matching_object X (obj x) :=
     begin
       let C' := C_without_z z,
       let X' := Functor_from_C' z X,
-      let  ψ := @red_coslice_without_z_equiv C z x φ inj_φ max_rank _ _,
+      let  ψ := @red_coslice_without_z_equiv C z _ φ inj_φ max_rank _ _,
       
       -- we got two commuting triangles using equivalence  ((obj x)//C) ≃ (x//C')
       assert Htr1 : ∀ y, (X' ∘f !forget) (@to_fun _ _ ψ y) = (X ∘f !forget) y,
         begin intro, unfold functor.compose, unfold forget, unfold Functor_from_C',
-          unfold red_coslice_obs.to_coslice_obs, esimp, refine red_coslice_eq_1 _ _, assumption 
+          unfold red_coslice_obs.to_coslice_obs, esimp, refine red_coslice_eq_1 _ _,
         end,      
       assert Htr2 : ∀ y, (X ∘f !forget) (@inv_fun _ _ ψ y) = (X' ∘f (!forget)) y, 
         begin intro, unfold functor.compose, unfold forget, unfold Functor_from_C',
-
       unfold red_coslice_obs.to_coslice_obs, cases y with [y,f,y_ne], esimp end,
       unfold matching_object,
       refine equiv.mk (@MO'_to_MO_map _ _ _ _ inj_φ max_rank _ _ X) _ _ _,      
-      { intros a, cases a, refine natural_transformation.mk _ _,
-        intros y uu, unfold functor.compose at *, unfold forget at *, esimp at *,
-        rewrite -Htr2, refine η (@inv_fun _ _ ψ y) star,
-        apply sorry },
-      { intros a, refine nat_trans_eq, cases a with [η, NatSq], esimp, 
-        rewrite natural_map_proj, esimp,
-        apply funext, intro y, apply funext, intro uu,
-         esimp, cases uu, apply sorry
-          -- calc
-          --     Htr2 y ▹ Htr1 (@inv_fun _ _ _ y) ▹ η (@to_fun _ _ _ (@inv_fun (x//C) _ _ y)) star                 
-          --         = Htr1 (@inv_fun _ _ ψ y) ▹ η _ star : sorry -- apd _ (H2 _)
-          --     ... = η y star : sorry,
-              },
-      { apply sorry }      
+      { intros a, cases a with [η, NatSq], refine natural_transformation.mk _ _,
+        intros y uu, 
+        assert H1 :  poly_unit → (X∘f forget C (obj x)) (@equiv.inv _ _ ψ y), 
+        begin apply η end, intro, unfold functor.compose at *, unfold forget at *, unfold Functor_from_C' at *,
+        unfold red_coslice_obs.to_coslice_obs, cases y with [y,f,y_ne], esimp, apply H1 star,        
+        intros a b f,
+        assert HH : morphism (X∘f forget C (obj x)) (red_coslice_to_C'_hom _ _ f) ∘ η (red_coslice_to_C' a) 
+        = η (red_coslice_to_C' b), 
+        begin refine NatSq _ end,
+        unfold Functor_from_C' at *, unfold functor.compose at *, unfold forget at *, esimp at *,
+        unfold red_coslice_to_C'_hom at *, esimp at *,
+        cases f with [f,comm_tr], cases a with [a, ff, non_id_ff ], cases b with [b, gg, non_id_gg], esimp at *, unfold one_funct at *,
+        apply funext, intros uu, cases uu,
+        refine happly HH _},
+      { intros a, refine nat_trans_eq, cases a with [η, NatSq], 
+        esimp, rewrite natural_map_proj,
+        unfold MO'_to_MO_map, unfold fn, unfold equiv.inv, rewrite natural_map_proj,
+        esimp,
+        apply funext, intro y, apply funext, intro uu, cases uu,
+        assert Heq : (@to_fun _ _ ψ (@inv_fun (obj x//C) _ ψ y)) = y, begin apply right_inv end, cases Heq,
+        cases y with [y,f,non_id_f], esimp at *, cases x with [x,p], esimp,
+        },
+      { intros a, refine nat_trans_eq, cases a with [η, NatSq],
+        esimp, rewrite natural_map_proj,
+        unfold MO'_to_MO_map, rewrite natural_map_proj,
+        apply funext, intro y, apply funext, intro uu, cases uu,
+        cases y, cases x, esimp }
     end
 
   end MO_facts
