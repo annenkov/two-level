12905765692ea6f1ff23b51b1f5ab3e8cdb01a71
Danil <danil.v.annenkov@gmail.com>
2017-05-04 17:51:41 +0200

    Added comments about beta rules

--- types/sigma.lean
+++ types/sigma.lean
@@ -1,10 +1,13 @@
 import ..fibrant ..facts
 /- Ported from the Lean HoTT library with some changes.
-   Facts about sigma-types harder to port as they use more
-   "generic" functionality from the HoTT library in comparison
+   Facts about sigma-types harder to port in a way they are implemented.
+   Proofs use more "generic" functionality from the HoTT library in comparison
    with product types.
 -/
-open sigma sigma.ops eq
+open sigma sigma.ops
+
+-- notation for the transport along the strict equality
+notation p `▹s` x := eq.rec_on p x
 
 namespace hsigma
 
@@ -25,6 +28,17 @@ namespace hsigma
       induction q, reflexivity
     end
 
+  -- it is awkward to define such computation rules, because we have to
+  -- manually use other computation rules in the statement itself
+  -- But it seems, that it is sufficient to define computation rules for
+  -- some base cases, and then just unfold definitions and use "simp" or
+  -- rewrite explicitly.
+
+  definition dpair_eq_dpair_β :
+    @dpair_eq_dpair _ _ a a _ b idp (eq.symm (subst_β b) ▹s idp) = idp :=
+    begin unfold dpair_eq_dpair, rewrite elim_β,
+    rewrite eq.transport_concat, simp end
+
   definition sigma_eq [reducible] (p : pr₁ u ~ pr₁ v) (q : p ▹ pr₂ u ~ pr₂ v) : u ~ v :=
     (sigma.cases_on u (λ x y, sigma.cases_on v (λ x1 y1, dpair_eq_dpair))) p q
 
@@ -32,16 +46,34 @@ namespace hsigma
     u ~ v -> Σ (p : pr₁ u ~ pr₁ v), p ▹ (pr₂ u) ~ pr₂ v :=
       elim ⟨refl u.1, eq.rec (refl u.2) (eq.symm (subst_β u.2))⟩ _
 
+  namespace eq
+    definition pathover_idp_of_eq [reducible] {A : Fib} {B : A → Fib} {a : A} {b b' : B a} :
+      b ~ b' → (refl a) ▹ b ~ b' :=  λ p, by induction p; rewrite subst_β
+
+    definition pathover_idp_of_eq_β {A : Fib} {B : A → Fib} {a : A} {b : B a} :
+      @pathover_idp_of_eq A _ _ _ _ (refl b) = (eq.symm (subst_β b) ▹s idp) :=
+      by simp
+
+  end eq
+
 /- Applying dpair to one argument is the same as dpair_eq_dpair with reflexivity in the first place. -/
+
   open fib_eq.ap
-  definition pathover_idp_of_eq {A : Fib} {B : A → Fib} {a : A} {b b' : B a} :
-    b ~ b' → (refl a) ▹ b ~ b' :=  λ p, by induction p; rewrite subst_β
 
   definition ap_dpair (q : b₁ ~ b₂) :
-  ap (sigma.mk a) q ~ dpair_eq_dpair idp (pathover_idp_of_eq q) :=
+  ap (sigma.mk a) q ~ dpair_eq_dpair idp (eq.pathover_idp_of_eq q) :=
   begin
-    induction q, unfold pathover_idp_of_eq, unfold dpair_eq_dpair,
-    repeat rewrite elim_β, rewrite transport_concat
+    induction q, unfold eq.pathover_idp_of_eq, unfold dpair_eq_dpair,
+    repeat rewrite elim_β, rewrite eq.transport_concat
   end
+
+  -- proof of the same property as above, but using propositional computation rules for
+  -- the definitions involved in the type
+  definition ap_dpair' (q : b₁ ~ b₂) :
+    ap (sigma.mk a) q ~ dpair_eq_dpair idp (eq.pathover_idp_of_eq q) :=
+    begin
+    induction q, rewrite eq.pathover_idp_of_eq_β, rewrite dpair_eq_dpair_β,
+    rewrite ap_β
+   end
 
 end hsigma
